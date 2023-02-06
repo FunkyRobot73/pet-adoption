65a2d11ed46e63b04a8e31338b7f4203d77d300d
Carlos <arlo252@gmail.com>
2023-02-05 20:59:00 -0500

    add gallery & new banner

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
+  res.render(__dirname + "index")
 });
 
-app.get("/adopt", function(req, res){
-  res.render(__dirname + "/adopt.html")  
-});
 
 
 app.listen(process.env.PORT || 3000, function() {
